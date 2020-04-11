20e85cd3f9aac10f5d8aed84a51dc0d8eb3508a2
Santahero007 <63391916+Santahero007@users.noreply.github.com>
2020-04-11 13:28:57 +0700

    Assignments

---
+++ Exercise4_Kritsanuphong_H.py
@@ -0,0 +1,10 @@
+FoundEng = 40
+GenBus = 60
+IntroCom = 50
+ComPro = 70
+
+print("---Your Score---")
+print("Foundation English",':',FoundEng,"/",100)
+print("General Business",':',GenBus,"/",100)
+print("Introduction to Computer Systems",':',IntroCom,"/",100)
+print("Computer Programming",':',ComPro,"/",100)
