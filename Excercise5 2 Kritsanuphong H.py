978ff9fd7ccdea2f0795947a47cb82237486a13e
Santahero007 <63391916+Santahero007@users.noreply.github.com>
2020-04-12 15:49:46 +0700

    Excercise5

---
+++ Excercise5 2 Kritsanuphong H.py
@@ -0,0 +1,4 @@
+distance = int(input("Distance(km) = "))
+time = int(input("Time in Hour(h) = "))
+result = distance / time
+print("Velocity :",result,"km/h")
---
+++ Exercise5 1 Kritsanuphong H.py
@@ -0,0 +1,6 @@
+no1 = int(input("Choose Your First Number"))
+no2= int(input("Choose Your Second Number"))
+print(no1,"+",no2,"=",(no1+no2))
+print(no1,"-",no2,"=",(no1-no2))
+print(no1,"*",no2,"=",(no1*no2))
+print(no1,"/",no2,"=",(no1/no2))
