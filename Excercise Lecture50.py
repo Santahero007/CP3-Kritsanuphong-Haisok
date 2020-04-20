b9c80e2e3e6e178b808520bffa78e71933d6b814
Santahero007 <63391916+Santahero007@users.noreply.github.com>
2020-04-20 13:13:12 +0700

    Add files via upload

---
+++ Excercise Lecture50.py
@@ -0,0 +1,19 @@
+def plus(x,y):
+    print(x+y)
+plus(10,5)
+
+def minus(x,y):
+    print(x-y)
+minus(20,10)
+
+def multiplied(x,y):
+    print(x*y)
+multiplied(2,2)
+
+def divide(x,y):
+    print(x/y)
+divide(10,5)
+
+
+
+
